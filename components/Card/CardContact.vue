c8481472b11615d21157900ca839da1cb97d89fd
Alexandre Zenon <a.zenon@student.uclouvain.be>
2021-02-11 19:01:12 +0100

    Modified contact card

--- components/Card/CardContact.vue
+++ components/Card/CardContact.vue
@@ -1,30 +1,11 @@
 <template>
   <card title="Contact">
-    <form
-      class="flex flex-col px-4 pt-6 pb-4 flex-grow"
-      name="contact"
-      method="POST"
-    >
-      <label for="email" class="font-semibold text-lg dark:text-white"
-        >Email</label
-      >
-      <input
-        type="email"
-        id="email"
-        name="email"
-        class="border rounded-md shadow-inner"
-      />
-      <label for="body" class="font-semibold text-lg mt-2 dark:text-white"
-        >Your message</label
-      >
-      <textarea name="body" class="border rounded-md shadow-inner flex-grow" />
-      <button
-        type="submit"
-        class="mt-4 p-2 rounded-md font-light shadow-inner bg-purple-800 text-white dark:bg-green-300 dark:text-black transform hover:scale-95 transition duration-300"
-      >
-        Contact me
-      </button>
-    </form>
+    <list class="p-4">
+      <list-element
+        title="Email"
+        link="mailto:Ahzed11@protonmail.com"
+      >Ahzed11@protonmail.com</list-element>
+    </list>
   </card>
 </template>
 
